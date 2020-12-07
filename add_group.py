efe9084965d4031ff276c834fa9fda58274b7039
Ryan <rpere16@gmail.com>
2020-12-07 14:42:38 -0600

    Update add_group.py

--- add_group.py
+++ add_group.py
@@ -6,8 +6,8 @@ import sys
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-key = 'LUFRPT1wVlIxZVB1V0FrNjdoTHRGcy9NZndQanNLT3M9WDVSNUFyV05wZnZETjh3L2d6Wkx4dUttQjFlN2J5K2FWYjV0bzVYY2lxTDR0emxsMjVPY21sR3NxUGpjRDVCeg=='
-ip = '192.168.1.48'
+key = '<KEY>'
+ip = '1.1.1.1'
 FILENAME = 'groups.json'
 
 
@@ -20,12 +20,9 @@ def open_json_file():
 def send_obj(grpName,grpMembers):
     tp = Template("https://{{ ip }}/api/?key={{ key }}&type=config&action=set&xpath=/config/shared/address-group/entry[@name='{{ grpName }}']/static &element={{ grpMembers }}")
     msg = tp.render(ip=ip,key=key,grpName=grpName,grpMembers=grpMembers)
-    print(msg)
     s = requests.session()
     s.verify=False
     r = s.get(msg)
-    print(r.text)
-
 
 data = open_json_file()
 for (k,v) in data.items():
